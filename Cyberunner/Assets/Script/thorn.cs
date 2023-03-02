5639de789fc490b3c75f9c3ada2060af76bcf11f
Iceberg05 <118051873+Iceberg05@users.noreply.github.com>
2023-03-02 23:39:27 +0300

    diken hareket kodları
    2.03.2023

---
+++ Cyberunner/Assets/Script/thorn.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thorn : MonoBehaviour
+{
+
+    [SerializeField] private Transform[] _waypoints;
+    [SerializeField] private float _speed;
+
+    private Transform _targetWaypoint;
+    private int _currentWaypointIndex = 0;
+    [SerializeField] private float _checkDistance = 0.05f;
+
+    void Start()
+    {
+        _targetWaypoint = _waypoints[0];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = (Vector3)Vector2.MoveTowards(
+
+            current: (Vector2)transform.position,
+            (Vector2)_targetWaypoint.position,
+            maxDistanceDelta: _speed * Time.deltaTime
+      );
+
+        if (Vector2.Distance(a: transform.position, b: _targetWaypoint.transform.position) < _checkDistance)
+        {
+            _targetWaypoint = GetNextWaypoint();
+        }
+
+
+    }
+
+
+    private Transform GetNextWaypoint()
+    {
+        _currentWaypointIndex++;
+        if (_currentWaypointIndex >= _waypoints.Length)
+        {
+            _currentWaypointIndex = 0;
+        }
+
+        return _waypoints[_currentWaypointIndex];
+    }
+
+
+}
