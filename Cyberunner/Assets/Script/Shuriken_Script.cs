25320651cbde0225f8e518499f9997445b07e7fa
Iceberg05 <118051873+Iceberg05@users.noreply.github.com>
2023-03-04 01:13:14 +0300

    1 13
    1 13

--- Cyberunner/Assets/Script/Shuriken_Script.cs
+++ Cyberunner/Assets/Script/Shuriken_Script.cs
@@ -1,29 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Camera))]
 public class Shuriken_Script : MonoBehaviour
 {
-    public GameObject crosshairs;
+    public Transform Crosshair;
+    private Camera cameraa;
     public GameObject player;
     public GameObject bulletPrefab;
     public GameObject bulletStart;
-    public float bulletSpeed = 15.0f;
+    public float bulletSpeed = 60.0f;
 
     private Vector3 target;
 
     // Use this for initialization
     void Start()
     {
+        cameraa = GetComponent<Camera>();
         Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
-        crosshairs.transform.position = new Vector2(target.x, target.y);
 
+
+
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        Crosshair.transform.position = new Vector2(target.x, target.y);
+        
         Vector3 difference = target - player.transform.position;
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         player.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
