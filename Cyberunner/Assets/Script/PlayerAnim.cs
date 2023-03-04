5fa829956baf1be0132da9343372b6e7fe62b180
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-03-04 14:29:01 +0300

    Player walking animaiton

---
+++ Cyberunner/Assets/Script/PlayerAnim.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    Animator anim;
+    void Start()
+    {
+      anim = GetComponent<Animator>();      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0){
+            anim.SetBool("Run" , true);
+        }
+        else{
+            anim.SetBool("Run" , false);
+        }
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            anim.SetBool("Attack" , true);
+        }
+        else
+        {
+            anim.SetBool("Attack", false);
+
+        }
+       
+    }
+}
