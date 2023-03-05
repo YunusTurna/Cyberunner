82b12df7b0471e464ad38aaaffbc839e15601629
Ahmet Yunus Turna <ahmetyunusturna@hotmail.com>
2023-03-05 17:01:40 +0300

    UI

--- Cyberunner/Assets/Script/Button.cs
+++ Cyberunner/Assets/Script/Button.cs
@@ -7,9 +7,11 @@ public class Button : MonoBehaviour
 {
     [SerializeField] GameObject world;
     [SerializeField] GameObject market;
+    AudioSource au;
+    private int SoundSetting;
     void Start()
     {
-        
+        au = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -39,6 +41,22 @@ public class Button : MonoBehaviour
         Cursor.visible = false;
 
 
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("Tutorial");
+
+    }
+    
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void Sound()
+    {
+        {
+            au.mute = !au.mute;
+        }
     }
 
 }
